92fded5897f99b3fe37e2e16ebebf7529ce9dca1
kyle1james <kyle1james@gmail.com>
2023-03-30 12:38:32 -0400

    cleaned up :)

--- robot/app.py
+++ robot/app.py
@@ -6,13 +6,22 @@ import io
 import time
 
 app = Flask(__name__)
-#GPIO.setmode(GPIO.BOARD)
-PIR_PIN = 26
-#GPIO.setup(PIR_PIN, GPIO.IN)
 
-# Set up the GPIO pin
+# TODO Setup fan controls
+
+# PIR MOTION SENSOR
+PIR_PIN = 26
 GPIO.setmode(GPIO.BCM)
 GPIO.setup(PIR_PIN, GPIO.IN)
+
+# Servo 1 -> orientation
+servo_pin = 18 # GPIO pin connected to servo signal wire
+freq = 50 # PWM frequency in Hz
+duty_min = 2.5 # duty cycle for minimum servo position in percent
+duty_max = 12.5 # duty cycle for maximum servo position in percent
+GPIO.setup(servo_pin, GPIO.OUT)
+
+
 
 @app.route('/')
 def index():
@@ -21,14 +30,6 @@ def index():
 @app.route('/pir_data')
 def pir_data():
     return Response(generate_pir_data(), mimetype='text/event-stream')
-
-def get_frame(camera):
-    stream = io.BytesIO()
-    camera.capture(stream, format='jpeg', use_video_port=True)
-    frame = stream.getvalue()
-    stream.seek(0)
-    stream.truncate()
-    return frame
 
 @app.route('/video_feed')
 def video_feed():
@@ -61,8 +62,9 @@ def sensor_data():
         # Clean up the GPIO pins
         GPIO.cleanup()
 
-@app.route('/run_agent',methods=['POST'])
-def run_agent():
+# TODO pipe jiji through here
+@app.route('/run_jiji',methods=['POST'])
+def run_jiji():
     script_output = subprocess.check_output(['python', 'scripts/test.py'])
     return render_template('result.html', output=script_output)
 
@@ -83,6 +85,21 @@ def generate_pir_data():
         yield f"data: {pir_state}\n\n"
         time.sleep(0.1)
 
-    
+def get_frame(camera):
+    stream = io.BytesIO()
+    camera.capture(stream, format='jpeg', use_video_port=True)
+    frame = stream.getvalue()
+    stream.seek(0)
+    stream.truncate()
+    return frame
+
+def set_servo_pos(pos):
+    # setup PWM
+    pwm = GPIO.PWM(servo_pin, freq)
+    pwm.start(0)
+    duty = duty_min + (pos/180)*(duty_max - duty_min)
+    pwm.ChangeDutyCycle(duty)
+    time.sleep(0.3) # wait for servo to reach position
+
 if __name__ == '__main__':
     app.run(host='0.0.0.0', debug=True)
