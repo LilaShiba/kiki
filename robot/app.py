2702a820bc4010928635fb1eae22f2bd76ca7e42
kyle1james <kyle1james@gmail.com>
2023-04-06 12:43:05 -0400

    woof

--- robot/app.py
+++ robot/app.py
@@ -21,6 +21,7 @@ camera.resolution = (640, 480)
 camera.framerate = 30
 # camera.brightness = 70
 # camera.contrast = 30
+IR_LED_PIN = 17  # The GPIO pin number where the IR LED is connected
 
 def gen():
     camera.start_preview()
@@ -55,6 +56,15 @@ def set_servo_pos(pos):
     pwm.ChangeDutyCycle(duty)
     time.sleep(0.3) # wait for servo to reach position
 
+def send_ir_signal(pulses):
+    GPIO.setmode(GPIO.BCM)
+    GPIO.setup(IR_LED_PIN, GPIO.OUT)
+    for pulse in pulses:
+        GPIO.output(IR_LED_PIN, GPIO.HIGH)
+        time.sleep(pulse[0] / 1000000.0)
+        GPIO.output(IR_LED_PIN, GPIO.LOW)
+        time.sleep(pulse[1] / 1000000.0)
+
 # Route for the main page
 @app.route('/')
 def index():
@@ -81,9 +91,35 @@ def capture():
 def stream():
     return Response(get_sensor_data())
 
-@app.route('/pir_data')
-def pir_data():
-    return Response(generate_pir_data(), mimetype='text/event-stream')
+@app.route('/pir')
+def pir():
+    if GPIO.input(PIR_PIN):
+        data = {'status': 'Motion detected'}
+    else:
+        data = {'status': 'No motion'}
+    return render_template('pir.html', data=data)
+
+@app.rout('/ir')
+def ir():
+    while True:
+        # Send the IR signal for the selected device
+        for device, value in off_signals.items():
+            send_ir_signal(off_signals[device])
+            time.sleep(1)  # Wait for 1 second before sending another signal
+        cycle = {'status': 'complete'}
+        return render_template('ir.html',data=cycle)
+    
+
+# IR pulses for various off commands
+off_signals = {
+    'SPEAKERS': [(800, 400), (800, 1600), (800, 400), (800, 1600), (800, 400), (800, 1600)],
+    'BLUETOOTH': [(350, 175), (350, 525), (350, 175), (350, 525), (350, 175), (350, 175), (350, 525)],
+    'TV': [(9000, 4500), (600, 550), (600, 1700), (600, 550), (600, 1700), (600, 550), (600, 1700), (600, 550), (600, 1700), (600, 550), (600, 550), (600, 550), (600, 550), (600, 550), (600, 550), (600, 1700)],
+    'SOUND_BAR': [(9000, 4500), (450, 450), (450, 1300), (450, 450), (450, 1300), (450, 450), (450, 1300), (450, 450), (450, 450), (450, 450), (450, 450), (450, 450), (450, 450), (450, 450), (450, 450), (450, 450), (450, 450), (450, 1300)],
+    'AMPLIFIER': [(9000, 4500), (600, 550), (600, 1700), (600, 550), (600, 1700), (600, 550), (600, 1700), (600, 550), (600, 1700), (600, 550), (600, 550), (600, 550), (600, 550), (600, 550), (600, 1700)],
+    #'AIR_CONDITIONER': [(3500, 1750), (550, 550), (550, 550), (550, 550), (550, 550), (550, 550), (550, 550), (550, 550), (550, 550), (550, 1650)]
+}
+
 if __name__ == "__main__":
     app.run(host='0.0.0.0', port=8000, debug=False)
 
