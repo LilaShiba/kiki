fe9727c3767b7ac88d995471dca3bc9eb2b53586
kyle1james <kyle1james@gmail.com>
2023-04-10 18:20:37 -0400

    btn dev

--- switch_test.py
+++ switch_test.py
@@ -1,33 +1,16 @@
 import RPi.GPIO as GPIO
 import subprocess
-import time
-
 SWITCH_PIN = 26
 # Set up the GPIO pin
 GPIO.setmode(GPIO.BCM)
-GPIO.setup(SWITCH_PIN, GPIO.IN)
+GPIO.setup(SWITCH_PIN, GPIO.IN, pull_up_down=GPIO.PUD_UP)
+
 # Define the function to run the program
 def run_program():
+    print('meow meow meow')
     #subprocess.call(["/path/to/your/program"])
-    print('me-wow!')
 
 # Wait for the switch to be pressed
-continous = 0
 while True:
-    # read the state of the channel
-    input_state = GPIO.input(SWITCH_PIN)
-    # perform some action based on the input state
-    if input_state == GPIO.LOW:
-        flag = True 
-    else:
-        flag = False
-    
-    continous += 1
-    if flag:
-        continous = 0
-
-    if continous > 150:
-        print('meow woof woof')
-        
-
-                              
+    if GPIO.input(SWITCH_PIN) == GPIO.HIGH:
+        run_program()
