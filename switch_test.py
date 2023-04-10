000b2e523bb499476a11dd09a01f9c1676cda8e2
kyle1james <kyle1james@gmail.com>
2023-04-10 17:43:39 -0400

    switch dev

--- switch_test.py
+++ switch_test.py
@@ -9,10 +9,10 @@ GPIO.setup(SWITCH_PIN, GPIO.IN)
 # Define the function to run the program
 def run_program():
     #subprocess.call(["/path/to/your/program"])
-    print('meeeeeeeoooooooow!')
+    print('me-wow!')
 
 # Wait for the switch to be pressed
 while True:
-    if GPIO.input(SWITCH_PIN) == GPIO.HIGH:
+    if GPIO.input(SWITCH_PIN) == GPIO.LOW:
         run_program()
 
