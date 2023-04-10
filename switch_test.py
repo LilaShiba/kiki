90bb10efb2872b390cec51b9a9dc3331152cd016
kyle1james <kyle1james@gmail.com>
2023-04-10 17:40:41 -0400

    switch dev

--- switch_test.py
+++ switch_test.py
@@ -1,16 +1,18 @@
 import RPi.GPIO as GPIO
 import subprocess
 
+SWITCH_PIN = 16
 # Set up the GPIO pin
 GPIO.setmode(GPIO.BCM)
-GPIO.setup(16, GPIO.IN, pull_up_down=GPIO.PUD_UP)
+GPIO.setup(SWITCH_PIN, GPIO.IN)
 
 # Define the function to run the program
 def run_program():
-    subprocess.call(["/path/to/your/program"])
+    #subprocess.call(["/path/to/your/program"])
+    print('meeeeeeeoooooooow!')
 
 # Wait for the switch to be pressed
 while True:
-    if GPIO.input(18) == GPIO.LOW:
+    if GPIO.input(SWITCH_PIN) == GPIO.LOW:
         run_program()
 
