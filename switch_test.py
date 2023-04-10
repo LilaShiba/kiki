8296a4fa01f117ab38a848c0934307d61fadad2d
kyle1james <kyle1james@gmail.com>
2023-04-10 18:18:28 -0400

    btn dev

--- switch_test.py
+++ switch_test.py
@@ -6,26 +6,27 @@ SWITCH_PIN = 26
 # Set up the GPIO pin
 GPIO.setmode(GPIO.BCM)
 GPIO.setup(SWITCH_PIN, GPIO.IN)
-continous = 0
 # Define the function to run the program
 def run_program():
     #subprocess.call(["/path/to/your/program"])
     print('me-wow!')
 
 # Wait for the switch to be pressed
-
+continous = 0
 while True:
     # read the state of the channel
     input_state = GPIO.input(SWITCH_PIN)
     # perform some action based on the input state
     if input_state == GPIO.LOW:
-        flag = True
+        flag = True 
     else:
         flag = False
+    
     continous += 1
     if flag:
         continous = 0
-    if continous > 50:
-         print("Button is pressed")
+
+    if continous > 150:
+        
 
                               
